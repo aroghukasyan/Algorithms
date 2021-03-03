8efd406aeef77da994c8858a31fd5f5ffea18f24
aro_programer <aro.programer@gmail.com>
2021-03-03 17:09:45 +0400

    Palindrome v_1

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+
+namespace Palindrome_02_03_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = null;
+            while (true)
+            {
+                Console.Write("Enter your text for cheking: ");
+                str = Console.ReadLine();
+                if (IsPalindrome(str))
+                    Console.WriteLine("text is Palindrom");
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("text is'nt Palindrom");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+        }
+        public static bool IsPalindrome(string str)
+        {
+            int length = str.Length;
+            int i = 0;
+            while (true) {
+                length--;
+                if (i >= length)
+                    break;
+                if (str[i] != str[length])
+                {
+                    return false;
+                }
+                i++;
+            } 
+            return true;
+        }
+    }
+}
